fb3fab499f031e46667abd69d8ec25d133ce7fc2
niharreddy1 <niharreddy1@outlook.com>
2021-10-29 10:36:39 -0500

    Finished initial design of the app.

---
+++ OneDrivePhotoApp/CleanUpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CleanUpViewController.swift
+//  OneDrivePhotoApp
+//
+//  Created by Nihar Reddy on 10/29/21.
+//
+
+import UIKit
+
+class CleanUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func triggerCleanUpSpace() {
+        print("Hello World!")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OneDrivePhotoApp/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  OneDrivePhotoApp
+//
+//  Created by Nihar Reddy on 10/28/21.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var userProfilePhoto: UIImageView!
+    @IBOutlet var userDisplayName: UILabel!
+    @IBOutlet var userEmail: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        // TEMPORARY
+        self.userProfilePhoto.image = UIImage(imageLiteralResourceName: "DefaultUserPhoto")
+        self.userDisplayName.text = "Default User"
+        self.userEmail.text = "default@contoso.com"
+    }
+
+    @IBAction func signOut() {
+        self.performSegue(withIdentifier: "userSignedOut", sender: nil)
+    }
+}
